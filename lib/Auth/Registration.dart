c3968ec378c254baf6d6a1a0f00737bbe78a405a
rsgamertv <Wersuser2@gmail.com>
2024-03-19 14:33:07 +0300

    saddsa

--- lib/Auth/Registration.dart
+++ lib/Auth/Registration.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class Registration extends StatefulWidget {
   const Registration({super.key});
